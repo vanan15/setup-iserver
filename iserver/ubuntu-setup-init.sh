6c5c102ee4b41cf7486680577b8ab1461d4eda60
vanan15 <vanan15@example.com>
2021-10-14 23:13:51 -0400

    Add ansible-lint

--- iserver/ubuntu-setup-init.sh
+++ iserver/ubuntu-setup-init.sh
@@ -3,4 +3,4 @@
 apt update -y
 apt install -y software-properties-common openssh-server python3-pip curl git
 add-apt-repository -y --update ppa:ansible/ansible
-apt install -y ansible
+apt install -y ansible ansible-lint
